ffd5005208d9feb91c9a64b18fdb6b8bdefc5eeb
NickMagic25 <37313679+NickMagic25@users.noreply.github.com>
2022-09-28 20:51:54 -0400

    findUser query added

--- db.js
+++ db.js
@@ -1,17 +1,22 @@
-var mysql = require('mysql');
+const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "csds-395-group4.co1rrt8vxy1n.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: 'admin',
-    password: "superman",
-    database: "insta-jacked"
-})
+function findUser(username, email, number, password) {
+    const sql = "SELECT * FROM user WHERE (user_name = '" + username + "' OR email = '" + email + "' or mobile_number "+
+        "= '" + number + "') AND hashed_password = '" + password + "'";
+    const connection = mysql.createConnection({
+        host: "csds-395-group4.co1rrt8vxy1n.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: 'admin',
+        password: "superman",
+        database: "insta-jacked"
+    })
+    connection.connect(function(err){
+        if (err) throw err;
+        connection.query(sql, function(err, result, fields){
+            if (err) throw err;
+            console.log(result);
+        })
+    })
+}
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("mysql connected");
-})
-
-
-module.exports = connection;
+findUser("user0", "0@gmail.com", "0", "secure password");
