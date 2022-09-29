ec1116c532b3356b940fec525d1f6690b26aaaf3
NickMagic25 <37313679+NickMagic25@users.noreply.github.com>
2022-09-29 19:50:48 -0400

    logging in added, correctly corresponds to a webpage

--- db.js
+++ db.js
@@ -1,22 +1,39 @@
-const mysql = require("mysql");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mysql = require('mysql');
 
-function findUser(username, email, number, password) {
+const app = express();
+app.use(express.json());
+
+app.listen('3000', () => {
+    console.log("Server started on port 3000")
+});
+
+const db = mysql.createConnection({
+    host: "csds-395-group4.co1rrt8vxy1n.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: 'admin',
+    password: "superman",
+    database: "insta-jacked"
+})
+
+app.get("/login", (reg, res) =>{
+    // hard code information in for now
+    `const username = reg.body.username;
+    const email = reg.body.username;
+    const number  = reg.body.username;
+    const password = reg.body.password;`
+    const username = "user0"
+    const email = "0@gmail.com"
+    const number = "0"
+    const password = "secure password"
     const sql = "SELECT * FROM user WHERE (user_name = '" + username + "' OR email = '" + email + "' or mobile_number "+
         "= '" + number + "') AND hashed_password = '" + password + "'";
-    const connection = mysql.createConnection({
-        host: "csds-395-group4.co1rrt8vxy1n.us-east-1.rds.amazonaws.com",
-        port: 3306,
-        user: 'admin',
-        password: "superman",
-        database: "insta-jacked"
-    })
-    connection.connect(function(err){
+    db.query(sql, function(err, result){
         if (err) throw err;
-        connection.query(sql, function(err, result, fields){
-            if (err) throw err;
-            console.log(result);
-        })
-    })
-}
-
-findUser("user0", "0@gmail.com", "0", "secure password");
+        console.log(result);
+        res.send("Found user0")
+})});
