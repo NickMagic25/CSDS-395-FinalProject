8db4da63344cf28e986774ca52c8b8f8d2fab217
Varun-Dutta <72882713+Varun-Dutta@users.noreply.github.com>
2022-10-15 12:33:23 -0400

    Landing Page
    Landing Page with dummy pages for other pages. Bug with Link in MainNavigation.js.

---
+++ landing_page/landing-page/src/components/layout/workouts/Workout.js
@@ -0,0 +1,25 @@
+import classes from "./Workout.module.css";
+import Card from "../../../ui/Card"
+
+function Workout(props) {
+  return (
+    <Card>
+      <li className={classes.item}>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title}></img>
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+
+        <div className={classes.actions}>
+          <button>To Workout</button>
+        </div>
+      </li>
+    </Card>
+  );
+}
+
+export default Workout;
