c1bad07788caa530fdea4ab0e3123798192e6c98
ramon-wang-rw <wangramon1@gmail.com>
2022-10-15 18:28:07 -0400

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
+import {BrowserRouter, Router, Routes, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  }
+  
   return (
-    <div className="container">
-      <Navbar />
-      <form onSubmit={handleSubmit}>
-        <h1>Login Form</h1>
-          <label for="username">UserName</label>
-          <input type="text" name="username" id="name" />
-          <button>Submit</button>
-      </form>
-    </div>
-  )
+  <BrowserRouter>
+    <Routes>
+      <Route>
+        <Route path='/' element={<Dashboard />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+
+    );
+    
+  
 }
 
 export default App;
