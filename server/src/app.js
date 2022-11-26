05ea4fed3d2fba03bc4c5457e72866334e8990d7
Nick <majkic65@gmail.com>
2022-11-26 14:14:12 -0500

    Move app.js

--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mysql = require('mysql');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('../../routes');
+var usersRouter = require('../../routes/users');
 
 var app = express();
 
