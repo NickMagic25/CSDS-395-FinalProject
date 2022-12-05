f6ed5a9b7a33cdc40be5a5a65687d456cc30fa67
Varun-Dutta <72882713+Varun-Dutta@users.noreply.github.com>
2022-12-05 17:40:17 -0500

    CSS Update
    Updated the CSS for the Landing Page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,31 +1,31 @@
-import Navbar from "/Users/varundutta/Desktop/Senior Project/CSDS-395-FinalProject/client/src/Components/navbar/Navbar.jsx";
+import Navbar from "../Components/navbar/Navbar";
 import {Container, Col, Row} from 'react-bootstrap'
-import format from './HomePage.css'
+import styles from './HomePage.module.css'
 import Deadlift from '../deadlift.png'
 import Social from '../social.jpeg'
 
 
 function HomePage(){
-    return <section className="homePage">
+    return <section>
       <Navbar/>
       <Container>
         <Row>
           <Col>
-          <h1 className="mainText">WHERE SHARING YOUR FITNESS JOURNEY BECOMES EASIER</h1>
-          <h2>THE GO-TO ONLINE PLATFORM FOR TRACKING AND SHARING YOUR WORKOUT JOURNEY WITH FRIENDS!</h2>
+          <h1 className={styles.h1Format}>WHERE SHARING YOUR FITNESS JOURNEY BECOMES EASIER</h1>
+          <h2 className = {styles.h2Format}>THE GO-TO ONLINE PLATFORM FOR TRACKING AND SHARING YOUR WORKOUT JOURNEY WITH FRIENDS!</h2>
           <form>
             <label>
                <input type="text" name="name" defaultValue={"Enter Email"}/>
                </label>
-                <input type="submit" value="Sign up" />
+                <input type="submit" value="Sign up" className={styles.btn}/>
                 </form>
 
-                <img src = {Social} alt = "Social media graph" className="img"></img>
+                <img src = {Social} alt = "Social media graph" className={styles.img}></img>
                 
                 
           </Col>
           <Col>
-          <img src = {Deadlift} alt ="Deadlift"></img>
+          <img src = {Deadlift} alt ="Deadlift" className={styles.deadLiftImage}></img>
           </Col>
         </Row>
       </Container>
