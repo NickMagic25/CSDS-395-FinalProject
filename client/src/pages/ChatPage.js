bba53b282609ece94123d13b03c44ed91245317d
manbudda <mrb204@case.edu>
2022-12-08 12:07:35 -0500

    manbudda3

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Login from './Login'
-import LocalStorage from '../../src/hooks/LocalStorage';
+import LocalStorage from '../hooks/LocalStorage';
 import Dashboard from '../Components/chat/Dashboard'
-import {ContactsProvider} from '../../src/contexts/chat/ContactsProvider';
+import {ContactsProvider} from '../contexts/chat/ContactsProvider';
 //import SideBar from './components/SideBar';
 // import "bootstrap/dist/css/bootstrap.min.css";
-import { MessagesProvider } from '../../src/contexts/chat/MessagesProvider';
-import {SocketProvider} from '../../src/contexts/chat/SocketProvider'
+import { MessagesProvider } from '../contexts/chat/MessagesProvider';
+import {SocketProvider} from '../contexts/chat/SocketProvider'
 
 function Chat() {
   const [id, setId] = LocalStorage('id')
